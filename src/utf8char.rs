48b3ac8a19749eec562f86291aad44ac58c13c99
Piotr Czarnecki <pioczarn@gmail.com>
2014-08-04 22:23:13 +0100

    Implement DoubleEndedIterator for Utf8Chars

--- src/utf8char.rs
+++ src/utf8char.rs
@@ -59,6 +59,24 @@ macro_rules! next {
     }
 }
 
+macro_rules! next_back {
+    ($iter:expr) => {
+        match $iter.next_back() {
+            Some(&b) => b,
+            None => return None,
+        }
+    }
+}
+
+/// Mask of the value bits of a continuation byte
+static CONT_MASK: u8 = 0b0011_1111u8;
+/// Value of the tag bits (tag mask is !CONT_MASK) of a continuation byte
+static TAG_CONT_U8: u8 = 0b1000_0000u8;
+
+macro_rules! utf8_is_cont_byte(
+    ($byte:expr) => (($byte & !CONT_MASK) == TAG_CONT_U8)
+)
+
 impl<'a> Iterator<Utf8Char> for Utf8Chars<'a> {
     #[inline]
     fn next(&mut self) -> Option<Utf8Char> {
@@ -75,6 +93,43 @@ impl<'a> Iterator<Utf8Char> for Utf8Chars<'a> {
         };
         unsafe {
             Some(Utf8Char::from_raw(buf))
+        }
+    }
+}
+
+#[inline]
+fn unwrap_or_0(opt: Option<&u8>) -> u8 {
+    match opt {
+        Some(&byte) => byte,
+        None => 0,
+    }
+}
+
+impl<'a> DoubleEndedIterator<Utf8Char> for Utf8Chars<'a> {
+    #[inline]
+    fn next_back(&mut self) -> Option<Utf8Char> {
+        let iter = &mut self.iter;
+        unsafe {
+            let ch = match iter.next_back() {
+                None => return None,
+                Some(&back_byte) if back_byte < 128 => {
+                    return Some(Utf8Char::from_raw([back_byte, 0, 0, 0]))
+                }
+                Some(&back_byte) => back_byte,
+            };
+
+            let ch2 = unwrap_or_0(iter.next_back());
+            if utf8_is_cont_byte!(ch2) {
+                let ch3 = unwrap_or_0(iter.next_back());
+                if utf8_is_cont_byte!(ch3) {
+                    let ch4 = unwrap_or_0(iter.next_back());
+                    Some(Utf8Char::from_raw([ch4, ch3, ch2, ch]))
+                } else {
+                    Some(Utf8Char::from_raw([ch3, ch2, ch, 0]))
+                }
+            } else {
+                Some(Utf8Char::from_raw([ch2, ch, 0, 0]))
+            }
         }
     }
 }
